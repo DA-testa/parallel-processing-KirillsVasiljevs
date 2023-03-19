fa8a9c1c875a7ae4ed1675564c6e39e108eb7136
KirillsVasiljevs <118903742+KirillsVasiljevs@users.noreply.github.com>
2023-03-19 11:34:03 +0000

    test

--- main.py
+++ main.py
@@ -1,30 +1,48 @@
 # python3
+# Kirills Vasiljevs 221RDB427
 
 def parallel_processing(n, m, data):
     output = []
-    # TODO: write the function for simulating parallel tasks, 
-    # create the output pairs
+    threads = []
+    for i in range(n):
+        threads.append(0)
+    i = 0
+    seconds = 0
+    while i < m:
+        free_thread_count = threads.count(0)
+        if free_thread_count > 0:
+            free_thread = threads.index(0)
+            threads[free_thread] = data[i]
+            output.append([free_thread, seconds])
+            i += 1
+        else:
+            seconds += 1
+            for j in range(n):
+                threads[j] -= 1
 
     return output
 
+
 def main():
-    # TODO: create input from keyboard
-    # input consists of two lines
-    # first line - n and m
-    # n - thread count 
+    thread_and_job_count = list(map(int, input().split()))
+    # n - thread count
+    n = thread_and_job_count[0]
     # m - job count
-    n = 0
-    m = 0
+    m = thread_and_job_count[1]
 
-    # second line - data 
+    # second line - data
     # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    data = []
+    data = list(map(int, input().split()))
+    assert len(data) == m
 
-    # TODO: create the function
-    result = parallel_processing(n,m,data)
+    # print out the results, each pair in it's own line
+    result = parallel_processing(n, m, data)
+
+    # threads take jobs
+    for i, j in result:
+        print(i, j)
+
     
-    # TODO: print out the results, each pair in it's own line
-
 
 
 if __name__ == "__main__":
